1006fae39e1f2392b770540412e68ca6075cfe61
KamalMajaiti <kamal@majaiti.es>
2017-02-27 22:44:09 +0100

    Rename Desactivar AntiRansomware.bat to DesactivarAntiRansomware.bat

--- DesactivarAntiRansomware.bat
+++ DesactivarAntiRansomware.bat
@@ -7,10 +7,10 @@ reg add "HKCU\Software\Microsoft\Windows Script Host\Settings" /v Enabled /t REG
 icacls "%userprofile%\AppData\Local\Microsoft\Windows\Temporary Internet Files\Content.IE5\" /grant *S-1-1-0:(OI)(IO)(X) 2> nul > nul
 icacls "%userprofile%\AppData\Local\Archivos temporales de Internet\Content.IE5\" /grant *S-1-1-0:(OI)(IO)(X) 2> nul > nul
 icacls "%userprofile%\AppData\Local\Temp" /grant *S-1-1-0:(OI)(IO)(X) 2> nul > nul
-icacls "%userprofile%\Configuración local\Temp" /grant *S-1-1-0:(OI)(IO)(X) 2> nul > nul
+icacls "%userprofile%\ConfiguraciÃ³n local\Temp" /grant *S-1-1-0:(OI)(IO)(X) 2> nul > nul
 icacls "c:\Documents and Settings\All Users" /grant *S-1-1-0:(OI)(IO)(X) 2> nul > nul
 icacls "%userprofile%\AppData\Local\Temp" /grant *S-1-1-0:(OI)(IO)(X) 2> nul > nul
-icacls "%userprofile%\Configuración local\Archivos temporales de Internet" /grant *S-1-1-0:(OI)(IO)(X) 2> nul > nul
+icacls "%userprofile%\ConfiguraciÃ³n local\Archivos temporales de Internet" /grant *S-1-1-0:(OI)(IO)(X) 2> nul > nul
 icacls "%userprofile%\AppData\Local\Microsoft\Windows\Temporary Internet Files" /grant *S-1-1-0:(OI)(IO)(X) 2> nul > nul
 icacls "%userprofile%\AppData\Local\Microsoft\Windows\INetCache" /grant *S-1-1-0:(OI)(IO)(X) 2> nul > nul
 icacls "%AppData%" /grant *S-1-1-0:(OI)(IO)(X) 2> nul > nul
